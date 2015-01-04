65c1974ef1965d8ddf250f6583276e862f9bc67c
CommitEveryday <supermegacoder@gmail.com>
2015-01-04 22:17:01 +0300

    draw cube with good normals

---
+++ CG_4_OpenGLScene/HelpersClasses/ErrorHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpGL;
+using System.Windows.Forms;
+
+namespace CG_4_OpenGLScene
+{
+    static class ErrorHandler
+    {
+        public static void TestError(OpenGL gl, RichTextBox log)
+        {
+            uint code;
+            StringBuilder str = new StringBuilder();
+            while ((code = gl.GetError()) != OpenGL.GL_NO_ERROR)
+            {
+                str.AppendLine(gl.GetErrorDescription(code));
+            }
+            if (str.Length > 0)
+            {
+                log.AppendText(str.ToString() + Environment.NewLine);
+            }
+        }
+    }
+}
